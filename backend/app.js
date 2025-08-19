9b153275907b4f0e49c40bfdf813066fdf52be7b
rori <rodte1234@gmail.com>
2025-08-19 07:39:52 -0600

    Arregle app

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://bluefruitnutrition1.onrender.com"],
     credentials: true,
   })
 );
