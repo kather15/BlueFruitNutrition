bd66e08db385c7801262637601eab27c83f83f9b
davidzepeda7 <davidzepeda1900@gmail.com>
2025-09-20 18:06:30 -0600

    Cambio Render

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://blue-fruit-nutrition-one.vercel.app", "https://blue-fruit-nutrition-private.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://blue-fruit-nutrition-git-master-bluefruitnutrition.vercel.app", "https://blue-fruit-nutrition-private.vercel.app"],
     credentials: true,
   })
 );
