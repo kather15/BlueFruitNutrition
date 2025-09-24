66d1552ca960da2d9af9cc4b324f5d6322c503c9
rori <rodte1234@gmail.com>
2025-09-24 08:07:33 -0600

    prueba 5

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -3,22 +3,7 @@ import loginController from "../controllers/CtrlLogin.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const token = await loginController.login(email, password);
 
-    // Cookie httpOnly, cross-site
-    res.cookie("authToken", token, {
-      httpOnly: true,
-      secure: true,    
-      sameSite: "none" 
-    });
-
-    res.json({ message: "Login exitoso" });
-  } catch (err) {
-    res.status(401).json({ message: "Credenciales inválidas" });
-  }
-});
+router.post("/", loginController.login);
 
 export default router;
