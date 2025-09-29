6c1a90996d872948deb3db2b877731e065c3c00b
Olga <olmenflo6@gmail.com>
2025-09-28 22:23:17 -0600

    Arreglo al carrito

--- frontend-public/src/pages/Carrito/Carrito.jsx
+++ frontend-public/src/pages/Carrito/Carrito.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/useAuth";
 import "./Carrito.css";
 
 const Carrito = () => {
